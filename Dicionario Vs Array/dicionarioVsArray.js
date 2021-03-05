b4e4b513631d7114356772556c37995f95ead727
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-03-05 19:04:18 -0300

    add

---
+++ Dicionario Vs Array/dicionarioVsArray.js
@@ -0,0 +1,17 @@
+//Array simples
+let frutas = ['Maça', 'pera', 'laranja']
+// Métodos mais usados 
+frutas.push('uva') //adicona itens no final
+frutas.pop(); // retira item no final
+frutas.reverse() // inverte
+
+console.log(frutas.toString())// transforma o array em texto
+console.log(frutas.join(' ')) // edita o texto
+console.log(frutas.join('-'))
+// console.log(frutas.length) tamanho do Array
+
+
+// lista de Dicionario
+let carro = [{nome:'corsa', cor:'prata'},{nome:'celta', cor:'preto'}];
+console.log(carro)
+console.log(carro[1].nome)
