1cc6b9d6242d5c667f36721343c13d7c43f72c80
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-04-18 20:11:24 -0300

    up

---
+++ eventos de pagina JS/formulario.js
@@ -0,0 +1,10 @@
+function focado(){
+console.log("Focado no campo!")
+}
+
+function desfocado(){
+console.log("Desfocou do campo!")
+}
+function opcaoSelect(objeto){
+    console.log(`Cidade selecionada: ${objeto.value}`) // parametro .ponto value(valor atual que foi passado para o parametro objeto)
+}
