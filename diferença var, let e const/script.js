36cd000392b9110f7f6ddfcf6384fbc4e661a059
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-04-19 20:47:07 -0300

    up

--- diferença var, let e const/script.js
+++ diferença var, let e const/script.js
@@ -2,7 +2,12 @@
 ou fora do escopo que foi criada
 
 
-let: Só pode ser acessado dentro do escopo que foi criado
+let: Só pode ser acessado dentro do escopo que foi criado, ele se resume ao seu escopo.
+Porque usar let ? por que protege de variaveis vazarem e ser chamadas em locais que não é
+desejado ou serem subescritas.
+
+Const: Como o nome ja diz é uma constante e seu primeiro valor armazenado não pode ser
+mudado.
 */ 
 // var
 var nome = 'juan'
@@ -19,9 +24,18 @@ if(altura === 1.80){
     let sobrenome = 'vitor'
      console.log(sobrenome)
 }
- console.log(sobrenome)/*
-O conselo log acima da erro, a variavel sobrenome foi criado como let
+console.log(sobrenome)/*
+O conselo.log acima da erro, a variavel sobrenome foi criado como let
  por isso vai dar erro dizendo 'sobrenome is not defined'.
 sobrenome só poderia ser chamando dentro do escopo que foi criado ou seja
 somento dentro do if
 */ 
+
+
+// Const
+
+const ceuCor = 'azul'
+console.log(ceuCor);
+//Atruido outro valor para ceuCor
+ceuCor = 'laranja'
+console.log(ceuCor); // dar erro dizendo que é uma constante e seu valor não pode ser mudado
