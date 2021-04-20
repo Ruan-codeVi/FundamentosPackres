b5a10426b9622409c3a8fc8f07e00926d1d43ed1
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-04-20 20:57:46 -0300

    desconstruindo Array's

---
+++ Desconstruindo Arrays - js/desconstArray.js
@@ -0,0 +1,27 @@
+// Desconstruindo Array's
+
+let pessoa = ['Juan','Vitor', 28, 1.80, 70]
+
+// Têm 2 modos de desconstruir um array
+// 1º Forma passando a posição do elemento no array.
+{
+let {1:sobreNome} = pessoa;
+console.log(sobreNome);
+// Descontruindo com mais de uma posição
+let {0: nome, 3: altura, 4:peso} = pessoa
+console.log(`nome: ${nome} altura: ${altura.toFixed(2)}, peso: ${peso}`);
+}
+/* 2º Criando um novo array com os nomes da varivaies desejadas 
+e atribuido ao array original. * Lembrar de seguir a ordem para a criação
+das novas variaveis com os elemento do array original.
+*/ 
+{
+let [primeiroNome, segundoNome, idade, altura, peso] = pessoa
+console.log('[]',primeiroNome, segundoNome, idade, altura.toFixed(2), peso)
+console.log('[]', segundoNome)
+}
+/* Pode-se desconstruir quantos elementos quiser, basta seguir a ordem dos
+elementos do array original para a criação dos nomes da variaves
+*/ 
+let [nome, sobreNome] = pessoa
+console.log(nome, sobreNome)
