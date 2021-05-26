738f7e22dea1ce708e70357378deef57049cb173
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-05-26 19:08:16 -0300

    up

--- Array's Métodos(itera, buscar etc..)/buscarElementoArray.js
+++ Array's Métodos(itera, buscar etc..)/buscarElementoArray.js
@@ -25,6 +25,45 @@ const numerosImpares = num.filter(item => item % 2 != 0)
 console.log('\nArray num', num)
 console.log('\nFilter no array num, só números impares', numerosImpares)
 
+const pets = [
+{
+    nome: 'bob',
+    tipo: 'Cachorro',
+    idade:10
+},
+{
+    nome: 'Lica',
+    tipo: 'Gato',
+    idade: 1
+},
+{
+    nome: 'Atum',
+    tipo: 'Peixe',
+    idade: 1
+
+}]
+// Formas diferente de usar a função de callback
+
+// 1ª Com função anonima
+const pet5idade = pets.filter(function(pet){
+    return pet.idade < 5
+})
+console.log('\nFilter com função anonima', pet5idade)
+
+// 2ª Utilizando arraow function com retorno imediato
+const petNovo = pets.filter(pet => pet.idade < 5)
+// console.log('\nFiltrando pet com idade menor que 5 anos\n', petNovo)
+
+// 3ª usando arrow func retornando um função declarada
+function petMenor5(numero){
+    return numero < 5
+}                             //destruction na prop
+const filtroPet = pets.filter(({idade}) => petMenor5(idade))
+// console.log('Usando destrution na prop idade', filtroPet)
+
+
+// * Existe outra formas ainda
+
 //----------------------------------------------------------------------------------------------------
 
 /* Método indexOf: Retorna o primeiro indice em que um elemento pode ser encontrado no array,
@@ -32,14 +71,14 @@ ou seja ele encontra a primeira ocorrência  e a sua posição no array. */
 
 const arr = [1,3,1,4,3]
 const primeiraOcorr = arr.indexOf(3)
-console.log('Primeira Ocorrência, index:', primeiraOcorr)
+console.log('\nPrimeira Ocorrência, index:', primeiraOcorr)
 
 //---------------------------------------------------------------------------------------------------------
 
 /* Método lastindexOf: Retorna o último indice em que um elemento pode ser encontrado no array */ 
 
 const ultimoOcorr = arr.lastIndexOf(3)
-console.log('Ultimo Ocorrência, index:', ultimoOcorr)
+console.log('\nUltimo Ocorrência, index:', ultimoOcorr)
 
 //------------------------------------------------------------------------------------------------------------
 
@@ -47,7 +86,7 @@ console.log('Ultimo Ocorrência, index:', ultimoOcorr)
 
 const existArr = arr.includes(3)
 const existArr01 = arr.includes(2)
-console.log('Existe no Array? booleano:', existArr, existArr01)
+console.log('\nExiste no Array? booleano:', existArr, existArr01)
 
 // ------------------------------------------------------------------------------------------------------------
 
@@ -57,7 +96,7 @@ console.log('Existe no Array? booleano:', existArr, existArr01)
     const alunos = [{nome: 'Joao', nota: 7},{nome: 'Lucas', nota: 5},
     {nome: 'Juan', nota: 10}]
     const passou = alunos.some(estudante => estudante.nota >=8)
-    console.log('Algum aluno passou',passou)
+    console.log('\nAlgum aluno passou',passou)
 
 
 // -------------------------------------------------------------------------------------------------------------
@@ -65,7 +104,7 @@ console.log('Existe no Array? booleano:', existArr, existArr01)
 /* Método every: Retorna um booleano verificando se todos os itens de um array satisfazem a condição */ 
 
 const todosPass = alunos.every(estudante => estudante.nota >= 6)
-console.log('Todos alunos passarão', todosPass)
+console.log('\nTodos alunos passaram?', todosPass)
 
 //-------------------------------------------------------------------------------------------------------------
 
@@ -76,10 +115,10 @@ Pedi um função callback que o primeiro parametro é item corrente e o segundo 
 */
 
 const menorPraMaior = alunos.sort((itemCorrent, proximoItem)=> itemCorrent.nota - proximoItem.nota)
-console.log('Ordenando notas da menor para a maior em array alunos', menorPraMaior)
+console.log('\nOrdenando notas da menor para a maior em array alunos', menorPraMaior)
 
 const maiorPraMenor = alunos.sort((itemCorrent, proximoItem)=> proximoItem.nota - itemCorrent.nota  )
-console.log('Ordenando notas da maior para a menor em array alunos', menorPraMaior)
+console.log('\nOrdenando notas da maior para a menor em array alunos', menorPraMaior)
 
 //--------------------------------------------------------------------------------------------------------
 
@@ -87,7 +126,7 @@ console.log('Ordenando notas da maior para a menor em array alunos', menorPraMai
 // Método reverse: inverte os elementos de um array
 let num03 = [1,2,3,4,5]
 let inverteArr = num03.reverse()
-console.log('Inverte o array num',inverteArr)
+console.log('\nInverte o array num',inverteArr)
 
 
 
