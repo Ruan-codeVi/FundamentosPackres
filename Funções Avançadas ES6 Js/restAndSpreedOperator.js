8e82d3af78ff513a2b4f65a2d3608065fb26668f
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-05-28 20:46:08 -0300

    update

--- Funções Avançadas ES6 Js/restAndSpreedOperator.js
+++ Funções Avançadas ES6 Js/restAndSpreedOperator.js
@@ -18,6 +18,8 @@ function somar(a,b){
  armazenados
 */
 
+                            //REST OPERATOR  ...
+
 // Está forma é mais declarativa
 function somar1(...args){ //rest operator + o nome dessa 'variavel rest'
     return args.reduce((somaTotal, num) => somaTotal + num, 0) //{
@@ -25,4 +27,59 @@ function somar1(...args){ //rest operator + o nome dessa 'variavel rest'
         // return somaTotal
     //},0)
 }
-console.log( somar1(2,3,5,4))
+ console.log('Soma total:', somar1(2,3,5,4))
+
+
+function multi (...numbers){
+    return numbers.reduce((valorAcum, num) => valorAcum * num,1)
+}
+console.log('Multiplicação total',multi(5,5,2,2))
+
+
+
+                           //SPREAD OPERATOR ...
+
+const frase = 'Digital innovation one'
+
+function showMess(...args){ //rest operator
+    console.log(args) // poedira passar arguments 
+}
+showMess(...frase) // spread operator
+/* Usando o rest operator na função para receber a quebra da string que está armazenada na varivel frase
+
+Na invocação da função showMess recebe como argumento o spread + variavel frase
+*/ 
+
+// Spread em Array
+const arrNUm = [1,2,3,4,5,6]
+function showNum(){ // poderia passar Rest
+    console.log('\n',arguments) 
+}
+showNum(...arrNUm)
+
+// Construindo Array's literais com Spread
+const num = [1,2,3,4]
+// Para juntar dois array existe o método concat mas com spread também dá fazer
+const num2= [...num, 5,6,7,8]
+const concaTArr = num.concat(['5','6','7','8'])
+console.log('\nUsando método Concat para juntar os arrays',concaTArr)
+console.log('\nJuntanto arrays com Spread operator',num2)
+const numbers01 = ['one','two', 'tree']
+const numbers02 = ['for','five','six']
+const newNumbArr = [...numbers01, ...numbers02, 'seven','eight','nine']
+console.log('\n',newNumbArr)
+
+/* Spread em objetos literais, só pode ser usado para copiar chaves e valores de outros objetos
+ para construir um novo objeto
+*/
+let object = {
+    prop: 123
+}
+let object02 ={
+    ...object,
+    newProp: 'Olá'
+}
+console.log('\nSpread em obj literal',object02)
+
+
+
