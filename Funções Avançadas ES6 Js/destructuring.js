f8a6e7ac9cb288c0749d41e3c565ea2270966c17
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-05-30 21:15:52 -0300

    update

--- Funções Avançadas ES6 Js/destructuring.js
+++ Funções Avançadas ES6 Js/destructuring.js
@@ -47,10 +47,58 @@ console.log('\n Antes do ES6,',name1)
 let obj1 ={
     nome: 'Eren'
 }
-let{nome} = obj1
+let{nome} = obj1 // para criar uma varivavel com nome diferente seria {nome: nome2} = obj1
 console.log('\n Depois do ES6,', nome)
 
 
+// Destructuring de obj dentro de outro objs
+
+// ANTES DO ES6
+
+let pessoa = {
+    nome:'juan',
+    prop: {
+        idade: 28
+    }
+}
+let idade = pessoa.prop.idade
+console.log('\n Objeto dentro de objeto, Antes do ES6,',idade)
+
+
+// APOS DO ES6
+
+/* Destructuring em  objetos literais
+    *Colocar o tipo de variavel let, var ou const
+    *Abrir chaves {}
+    * Colocar a propriedade que vai destruida, podendo usar o mesmo nome de prop para ser
+    o nome da nova variavel ou criando um novo nome pra essa variavel atraves de : dois pontos
+    e nome.
+    *Pode-se fazer destructuring em objetos que estão dentro de outros objetos, indicando o nome
+    da proprieade que cria esse obj, e para destruir prop desse objeto aninhado basta fazer
+    {obj:{}}
+*/ 
+
+let pessoa1 = {
+    nome1:'juan',
+    prop1: {
+        idade1: 28
+    }
+}
+let {prop1: {idade1}} = pessoa1 // poderia fazer mais destructuring e renomear as variaveis
+console.log('\nApos ES6 destructuring obj dentro de obj,', idade1)
+
+// Pode-se fazer destructuring em array's dentro de obj e vice e versa
+
+let maria ={
+    sobrenome: 'silva',
+    obj2: {
+        idade: 14,
+        coresFavoritas: ['Rosa','Roxo'] 
+    }
+}    //prop     //obj //renoneado prop          //destruibdo o array  
+let {sobrenome, obj2:{idade: anos, coresFavoritas:[corFav1, corFav2]}} = maria
+console.log('\ndestructuring em arrays que estão em obj,',sobrenome,anos, corFav1)
+console.log('destructuring em arrays que estão em obj,',corFav2)
 
 
 
