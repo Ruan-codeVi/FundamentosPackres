9c63a3987b3641d3e619f3940876ec657afaa1c5
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-06-02 20:25:07 -0300

    up

---
+++ Testes: TDD, BDD/testes.js
@@ -0,0 +1,16 @@
+/* TDD(Test Drive Development) - Desenvolvimento orientado a testes.
+É um dos pilares do Extreme Programming, consiste em testar e refatorar em pequenos ciclos, onde 
+você escreve o teste antes do código, faz o mesmo passar e refatora o código
+*/
+
+// Ciclo do TDD
+// 1º Escrita do teste
+// 2º Escrita de código
+// 3º Refatoração
+
+// Vantagens do uso de TDD
+/* * FeedBack rápido
+   * Maior segurança em alterações e novas funcionalidades
+   * Código mais limpo
+   * Produtividade
+*/ 
